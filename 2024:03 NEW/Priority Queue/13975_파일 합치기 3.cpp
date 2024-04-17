f87051d3b94d22002f2d8143231fb9e274673cc2
limsss9076@naver.com <limsss9076@naver.com>
2024-04-17 23:20:43 +0900

    4/17 우선순위큐 + 그리디

---
+++ 2024:03 NEW/Priority Queue/13975_파일 합치기 3.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+#include <queue>
+#include <cstdio>
+#include <set>
+#include <map>
+// #include <pair>
+// #include <bits/stdc++.h>
+
+
+// 13975 파일 합치기 3
+// 먼저 더한다 -> 더한 값이 계속해서 누적된다 => 더한 값이 작은 애들부터 더하자.
+
+// 1% 틀. -> long long
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+
+    int T; cin >> T;
+
+    while (T--) {
+
+        priority_queue<long long , vector<long long >, greater<long long > > pq;
+        int K; cin >> K;
+
+        for (int i = 0; i < K; i++) {
+            int x; cin >> x;
+
+            pq.push(x);
+        }
+
+
+        // 가장 작은 것 2개를 뽑아서 더한다. 더한 값은 그대로 pq에 넣어준다.
+        long long  sum = 0;
+        while (pq.size() > 1) {
+            long long n1 = pq.top(); pq.pop();
+            long long n2 = pq.top(); pq.pop();
+
+            long long n_sum = n1 + n2;
+
+            // cout << n1 << " *** " << n2 << "\n";
+
+            sum += n_sum;
+            pq.push(n_sum);
+        }
+
+        cout << sum << "\n";
+
+    }
+
+
+
+    return 0;
+}
