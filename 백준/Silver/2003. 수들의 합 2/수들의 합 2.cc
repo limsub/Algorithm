b22b1d77235f0590fc1f22f68d379a7eef315896
limsub <99518799+limsub@users.noreply.github.com>
2024-05-20 14:12:02 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+#include <queue>
+#include <cstdio>
+#include <set>
+#include <map>
+// #include <pair>
+// #include <bits/stdc++.h>
+
+// 2003 수들의 합 2
+// 투포인터
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+
+    // 1. 입력
+    int N, M; cin >> N >> M;
+    int arr[N+1];
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    int ansCnt = 0;
+
+    // 2. 투 포인터 알고리즘
+    int en = 0;
+    int sum = arr[0];   // arr[st] ~ arr[en]까지 합
+    for (int st = 0; st < N; st++){ 
+        while (sum < M) {
+            en++;
+            sum += arr[en];
+        }
+
+        if (sum == M) {
+            ansCnt++;
+        }
+
+        sum -= arr[st];
+    }
+
+    cout << ansCnt;
+
+    return 0;
+}
