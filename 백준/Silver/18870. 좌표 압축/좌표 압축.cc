aefc5d62debfabe2591af5f7983a3ad0055f72bb
limsub <99518799+limsub@users.noreply.github.com>
2024-05-25 15:20:18 +0900

    BakjoonHub (#3)
    * [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 7172 KB -BaekjoonHub
    
    * [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2156 KB -BaekjoonHub
    
    * [Silver II] Title: 좌표 압축, Time: 464 ms, Memory: 9832 KB -BaekjoonHub
    
    * [Gold IV] Title: 세 수의 합, Time: 36 ms, Memory: 5228 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+#include <queue>
+#include <cstdio>
+#include <set>
+#include <map>
+// #include <pair>
+// #include <bits/stdc++.h>
+
+
+// 18870 좌표 압축
+
+int arr[1000010];
+int arr2[1000010];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+
+    // 1. 입력
+    int N; cin >> N;
+    for (int i = 0; i < N; i++) {
+        int x; cin >> x;
+
+        arr[i] = x;
+        arr2[i] = x;
+    }
+
+    // 2. 정렬
+    sort(arr, arr + N);
+
+    // 2.5 중복 제거
+    int *new_end = unique(arr, arr + N);
+    int new_length = new_end - arr;
+
+
+    // 3. 좌표 압축?
+    // x1' : x1보다 작은 좌표들의 개수 
+    //  = x1이 등장하는 가장 작은 인덱스
+    
+    // 초기 세팅 필요
+    // 1. 정렬 2. 중복된 원소 제거
+    for (int i = 0; i < N; i++) {
+        int num = arr2[i];
+        int idx = lower_bound(arr, arr + new_length, num) - arr;
+
+        cout << idx << " ";
+
+        // ansArr.push_back(idx);
+    }
+     
+
+    return 0;
+}
