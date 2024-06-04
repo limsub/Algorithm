043d3631809f1c6ac83f2615f72d145cc1e895f5
limsub <99518799+limsub@users.noreply.github.com>
2024-06-04 22:15:32 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#define ll long long
+
+
+
+
+
+
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+
+    int x, y, z, w;
+    cin >> x >> y >> z >> w;
+    cout << x * y + z * w;
+
+
+}
+
