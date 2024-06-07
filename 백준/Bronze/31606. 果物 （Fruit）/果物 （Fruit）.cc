c8f91a50fd8f4c6fc1939e32b9c5f964d5358ae3
limsub <99518799+limsub@users.noreply.github.com>
2024-06-07 11:55:48 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#define ll long long
+
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+    
+    int x, y; cin >> x >> y;
+    cout << x + y + 3;
+
+
+    return 0;
+}
+
