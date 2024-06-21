0f3f26b2dbb3ee31579eaaca5fa153cbc731007d
limsub <99518799+limsub@users.noreply.github.com>
2024-06-21 20:18:55 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#define ll long long
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int x, y; cin >> x >> y;
+    
+    cout << (x + y) * (x - y);
+
+
+    return 0;
+}
