04397e8af1722143947f51caefe0f111e655d21e
limsub <99518799+limsub@users.noreply.github.com>
2024-10-25 09:41:20 +0900

    [Silver II] Title: 결혼식, Time: 0 ms, Memory: 2304 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.cc
+++ 백준/Silver/5567. 결혼식/결혼식.cc
@@ -1,47 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <stack>
-#include <sstream>
+#include <bits/stdc++.h>
 #include <algorithm>
 using namespace std;
-#include <queue>
-#include <cstdio>
-#include <set>
-#include <map>
-// #include <pair>
-// #include <bits/stdc++.h>
+#define ll long long
+#define INF 0x3F3F3F3F // INF + INF 연산할 일이 생길 수 있기 때문에 7로 안했다.
 
 
-// 5567 결혼식
-
-vector<int> adj[510];
+vector<int> arr[510];
 int v[510];
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0); 
+int n, m; 
 
-
-    int N; cin >> N;
-    int M; cin >> M;
-
-    for (int i = 0; i < M; i++) {
-        int x, y; cin >> x >> y;
-
-        adj[x].push_back(y);
-        adj[y].push_back(x);
-    }
-
+void bfs(int s) {
     queue<int> q;
-    q.push(1);
-    v[1] = 1;
+    q.push(s);
+    v[s] = 1;
 
     while (!q.empty()) {
         int cur = q.front(); q.pop();
 
-        for (int i = 0; i < adj[cur].size(); i++) {
-            int next = adj[cur][i];
+        for (int i = 0; i < arr[cur].size(); i++) {
+            int next = arr[cur][i];
 
             if (v[next]) continue;
 
@@ -50,12 +28,32 @@ int main() {
         }
     }
 
-    int cnt = 0;
-    for (int i = 1; i <= N; i++) {
-        if (v[i] <= 3 && v[i] != 0) cnt++;        
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int x, y; cin >> x >> y;
+        arr[x].push_back(y);
+        arr[y].push_back(x);
     }
 
-    cout << cnt - 1 << "\n";
+
+    bfs(1);
+
+    int ans = 0;
+    for (int i = 1; i < n + 1; i++) {
+        if (v[i] <= 3 && v[i] != 0) ans++;
+    }
+
+    cout << ans - 1;
+
+
+
 
     return 0;
 }
+
